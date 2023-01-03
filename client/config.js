baaf923bf1610fc7ff959307562f44049de96013
sfeeser <sfeeser@gmail.com>
2023-01-03 19:39:14 +0000

    stu work

---
+++ client/config.js
@@ -0,0 +1,12 @@
+var iceServers = [
+	{
+		urls       : 'turn:XXXX-XXXX:443?transport=tcp',
+		username   : 'websip',
+		credential : 'websip'
+	},
+	{
+		urls       : 'turns:XXXX-XXXX:80?transport=tcp',
+		username   : 'websip',
+		credential : 'websip'
+	}
+];
