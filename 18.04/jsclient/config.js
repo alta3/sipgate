182953fbbdcc4423fd9e28a1610dc8f7093cb6b7
seaneon <sean.r.barbour@gmail.com>
2023-01-31 09:53:09 -0500

    new dir struc

---
+++ 18.04/jsclient/config.js
@@ -0,0 +1,12 @@
+var iceServers = [
+	{
+		urls       : 'turn:sip.alta3.com:443?transport=tcp',
+		username   : 'websip',
+		credential : 'websip'
+	},
+	{
+		urls       : 'turns:sip.alta3.com:80?transport=tcp',
+		username   : 'websip',
+		credential : 'websip'
+	}
+];
