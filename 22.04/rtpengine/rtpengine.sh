d214a275f1ca89ee2747c35cd600aa2a07919b16
seaneon <35880398+seaneon@users.noreply.github.com>
2023-02-01 15:29:58 -0500

    Update rtpengine.sh

--- 22.04/rtpengine/rtpengine.sh
+++ 22.04/rtpengine/rtpengine.sh
@@ -1,8 +1,12 @@
 VER="1.1.1"
 
-git clone https://github.com/sipwise/rtpengine.git
-
 sudo apt install -y cmake debhelper init-system-helpers default-libmysqlclient-dev gperf libmysqlclient-dev libip4tc-dev libip6tc-dev libiptc-dev libxtables-dev libavcodec-dev libavfilter-dev libavformat-dev libavutil-dev libbencode-perl libcrypt-openssl-rsa-perl libcrypt-rijndael-perl libhiredis-dev libio-multiplex-perl libio-socket-inet6-perl libjson-glib-dev libdigest-crc-perl libdigest-hmac-perl libnet-interface-perl libnet-interface-perl libssl-dev libsystemd-dev libxmlrpc-core-c3-dev libcurl4-openssl-dev libevent-dev libpcap0.8-dev markdown unzip nfs-common dkms libspandsp-dev libjson-perl libmosquitto-dev libopus-dev libtest2-suite-perl libwebsockets-dev python3-websockets
+
+git clone https://github.com/alta3/sipgate.git
+
+cd ~/sipgate/22.04/rtpengine/
+
+git clone https://github.com/sipwise/rtpengine.git
 
 cd ~/sipgate/22.04/rtpengine/rtpengine
 
@@ -10,19 +14,19 @@ wget https://codeload.github.com/BelledonneCommunications/bcg729/tar.gz/$VER -O 
 
 tar zxf bcg729_$VER.orig.tar.gz
 
-cd bcg729-$VER
+cd ~/sipgate/22.04/rtpengine/bcg729-$VER
 
 git clone https://github.com/ossobv/bcg729-deb.git debian
 
-dpkg-buildpackage -us -uc -sa -b -rfakeroot
+sudo dpkg-buildpackage -us -uc -sa -b -rfakeroot
 
 cd ../
 
-dpkg -i libbcg729-*.deb
+sudo dpkg -i libbcg729-*.deb
 
-dpkg-checkbuilddeps
+sudo dpkg-checkbuilddeps
 
-dpkg-buildpackage -us -uc -sa
+sudo dpkg-buildpackage -us -uc -sa
 
 cd ../
 
