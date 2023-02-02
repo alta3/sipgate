86e9d5c98ac4f8f2da84c3dd06ca390e2620a91d
seaneon <35880398+seaneon@users.noreply.github.com>
2023-02-02 10:45:53 -0500

    Update make-test-cert.sh

--- 22.04/kamailio/make-test-cert.sh
+++ 22.04/kamailio/make-test-cert.sh
@@ -16,18 +16,30 @@ OUTPUT_DIR="$1"
 mkdir -p $OUTPUT_DIR
 cd $OUTPUT_DIR
 
-echo "generate CA private key"
+echo "Step 1: Generate CA private key"
+echo "-------------------------------"
 openssl genrsa -des3 -out ca.key 2048
 
-echo "generate root certificate"
+echo
+echo
+echo "Step 2: Generate CA root certificate"
+echo "------------------------------------"
 openssl req -x509 -new -nodes -key ca.key -sha384 -days 36500 -out ca.pem
 
-echo "generate a private key for certificates"
+echo
+echo
+echo "Step 3: Generate a private key for kamailio server"
+echo "--------------------------------------------------"
 openssl genrsa -out cert.key 2048
 
-echo "generate a certificate signing request"
+echo
+echo
+echo "Step 4: Generate kamailio server certificate signing request"
+echo "------------------------------------------------------------"
 openssl req -new -key cert.key -out cert.csr
 
+echo
+echo
 echo "create config file"
 >cert.ext cat <<EOF
 authorityKeyIdentifier=keyid,issuer
@@ -38,14 +50,29 @@ subjectAltName = @alt_names
 DNS.1 = localhost
 EOF
 
-echo "create unsigned certificate"
+echo "Step 5: Create unsigned certificate for kamailio server"
+echo "-------------------------------------------------------"
 openssl req -x509 -new -nodes -key cert.key -sha384 -days 36500 -out cert.unsigned.pem
 
-echo "create signed certificate"
+echo
+echo
+echo "Step 6: Use key created in step 2"
+echo "to sign the unsigned cert created"
+echo "in step 5."
+echo "---------------------------------"
 openssl x509 -req -in cert.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out cert.pem -outform PEM -days 36500 -sha384 -extfile cert.ext
 
-echo "store copy of certificate in pkcs12 format"
+echo
+echo
+echo "Step 7: Store copy of certificate in pkcs12 format"
+echo "pkcs12 format bundles private key with x509,"
+echo "creating a chain of trust."
+echo "--------------------------------------------------"
 openssl pkcs12 -export -out cert.p12 -in cert.pem -inkey cert.key
 
-echo "create expired certificate"
-openssl x509 -req -in cert.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out cert.expired.pem -days -1 -sha384 -extfile cert.ext
+echo
+echo
+echo "Step 8: Copying certs to kamailio directory."
+echo "--------------------------------------------"
+sudo cp ~/certs/cert.key /etc/kamailio/privkey.pem
+sudo cp ~/certs/cert.pem /etc/kamailio/fullchain.pem
