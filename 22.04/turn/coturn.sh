bdaf8fa9ccdffe71881407d5eb064d2be74ae76d
Stuart Feeser <sfeeser@gmail.com>
2023-02-02 11:03:46 -0500

    Update coturn.sh

--- 22.04/turn/coturn.sh
+++ 22.04/turn/coturn.sh
@@ -1,17 +1,16 @@
 `ssh to the turn server`
 
-`clone the sipgate repo`
+`sudo apt install git`
+
+`cd`
+
+`git clone https://github.com/alta3/sipgate.git`
 
 `sudo apt install -y coturn`
 
-`edit this file /etc/default/coturn`
-    
-        # Uncomment it if you want to have the turnserver running as
-        # an automatic system service daemon
-        #
-        TURNSERVER_ENABLED=1    
+`sudo cp ~/sipgate/22.04/coturn  /etc/default/coturn`
 
-`export $(TURN_IP4="magically determine the IP4 address of the active turn server, either turn.alpha.alta3.com or turn.bravo.alta3.com")`
+`export TURN_IP4=$(hostname -I | awk '{print $1}')`
 
 `export $(TURN_IP6="magically determine the IP6 address of the active turn server, either turn.alpha.alta3.com or turn.bravo.alta3.com")`
 
