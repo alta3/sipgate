de99e2bb6b52171a69b40b755a11657d7eab6e3e
seaneon <35880398+seaneon@users.noreply.github.com>
2023-02-03 09:37:56 -0500

    Update install-nginx.sh

--- 22.04/nginx/install-nginx.sh
+++ 22.04/nginx/install-nginx.sh
@@ -6,9 +6,9 @@ git clone https://github.com/alta3/sipgate.git
 
 sudo apt install nginx -y
 
-sudo rm /etc/nginx/sites-available/default.conf
+sudo rm /etc/nginx/sites-available/default
 
-sudo rm /etc/nginx/sites-enabled/default.conf
+sudo rm /etc/nginx/sites-enabled/default
 
 sudo cp /home/student/sipgate/22.04/nginx/webrtc.conf  /etc/nginx/conf.d/
 
