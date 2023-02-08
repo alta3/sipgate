2d7b5b43c64a9a17476a690fc814bb2c6471e852
seaneon <35880398+seaneon@users.noreply.github.com>
2023-02-08 13:18:41 -0500

    Update install-kamailio.sh

--- 22.04/kamailio/install-kamailio.sh
+++ 22.04/kamailio/install-kamailio.sh
@@ -1,13 +1,14 @@
 cd
 git clone https://github.com/alta3/sipgate.git
-sudo apt install mariadb-server kamailio  -y
+sudo apt install -y mariadb-server
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
-sudo apt install kamailio
-sudo apt install kamailio-mysql-modules
-sudo apt install kamailio-websocket-modules
-sudo apt install kamailio-tls-modules kamailio-extra-modules -y
-sudo apt install kamailio-json-modules -y
+sudo apt install -y kamailio
+sudo apt install -y kamailio-mysql-modules
+sudo apt install -y kamailio-websocket-modules
+sudo apt install -y kamailio-tls-modules
+sudo apt install -y kamailio-extra-modules
+sudo apt install -y kamailio-json-modules
 sudo systemctl enable kamailio
 export MY_IP4_ADDR=$(ip a s ens3 | awk -F"[/ ]+" '/inet / {print $3}')
 export MY_DOMAIN="sipgate.alta3.com"
@@ -18,5 +19,3 @@ j2 ~/sipgate/22.04/kamailio/kamctlrc.j2 > ~/sipgate/22.04/kamailio/kamctlrc
 sudo cp ~/sipgate/22.04/kamailio/kamailio.cfg /etc/kamailio/
 sudo cp ~/sipgate/22.04/kamailio/kamctlrc     /etc/kamailio/
 sudo cp ~/sipgate/22.04/kamailio/tls.cfg      /etc/kamailio/
-sleep 5
-sudo systemctl restart kamailio
