6b354597b41e1ad60b7f9cc708eaf81e8305ed8b
seaneon <35880398+seaneon@users.noreply.github.com>
2023-02-08 16:39:18 -0500

    Update sip-js.sh

--- 22.04/sip-js/sip-js.sh
+++ 22.04/sip-js/sip-js.sh
@@ -5,12 +5,12 @@ cd SipCaller/app
 sudo apt -y install npm
 npm install
 npm run build
+sudo mkdir -p /var/www/html/
 cd build/
 echo "Uncomment ONE of the two following lines depending on the turn server in use)
 export turn_ipv4="turn.alpha.alta3.com"
 #export turn_ipv4="turn.bravo.alta3.com"
 j2 ~/sipgate/22.04/content/config.js.j2 > ~/sipgate/22.04/content/config.js
-sudo mkdir -p /var/www/html/
 sudo cp ~/sipgate/22.04/content/config.js  /var/www/html/
 cd ..
 sudo cp -r * /var/www/html/
