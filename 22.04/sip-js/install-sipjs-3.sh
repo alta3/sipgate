c0e29615175e1f2fb6ada681ffcf45531252a0ab
seaneon <35880398+seaneon@users.noreply.github.com>
2023-02-10 11:22:55 -0500

    Create install-sipjs-3.sh

---
+++ 22.04/sip-js/install-sipjs-3.sh
@@ -0,0 +1,9 @@
+cd ~/SipCaller/app/build/
+echo "Uncomment ONE of the two following lines depending on the turn server in use)
+export TURN_CLOUD=$(nslookup 10.0.0.1 | grep -oP alpha||bravo)
+export TURN_FQDN="turn.$TURN_CLOUD.alta3.com"
+j2 ~/sipgate/22.04/content/config.js.j2 > ~/sipgate/22.04/content/config.js
+sudo cp ~/sipgate/22.04/content/config.js  /var/www/html/
+cd ..
+sudo cp -r * /var/www/html/
+sudo chown -R www-data:www-data /var/www/html/
