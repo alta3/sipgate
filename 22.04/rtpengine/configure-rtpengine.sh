e56620313a9ca7c4ac22ee00483b335992512b80
seaneon <35880398+seaneon@users.noreply.github.com>
2023-02-13 14:46:00 -0500

    Update configure-rtpengine.sh

--- 22.04/rtpengine/configure-rtpengine.sh
+++ 22.04/rtpengine/configure-rtpengine.sh
@@ -1,4 +1,15 @@
-if [[ -z "$(RTPE_PORT_MAX)" ]]
+echo "You will be prompted for RTPE_PORT_MIN and RTPE_PORT_MAX."
+echo "These values will be supplied by your instructor."
+echo
+read -p "Press Enter when ready."
+
+read -p "RTPE_PORT_MIN = " RTPE_PORT_MIN
+
+read -p "RTPE_PORT_MAX = " RTPE_PORT_MAX
+
+if [[ -z "$RTPE_PORT_MIN" ]] ; then  echo "ERROR: RTPE_PORT_MIN not set" ; fi
+
+if [[ -z "$RTPE_PORT_MAX" ]] ; then  echo "ERROR: RTPE_PORT_MAX not set" ; fi
 
 export TURN_CLOUD=$(nslookup 10.0.0.1 | grep -oP alpha||bravo)
 
@@ -7,3 +18,9 @@ export TURN_FQDN="turn.$TURN_CLOUD.alta3.com"
 export MY_OUTSIDE_IP=$(dig +short $TURN_FQDN)
 
 export MY_IP4_ADDR=$(ip a s ens3 | awk -F"[/ ]+" '/inet / {print $3}')
+
+j2 ~/sipgate/22.04/rtpengine/rtpengine.conf.j2 > ~/sipgate/22.04/rtpengine/rtpengine.conf
+
+sudo cp ~/sipgate/22.04/rtpengine/rtpengine.conf /etc/rtpengine/rtpengine.conf
+
+sudo systemctl restart ngcp-rtpengine-daemon
