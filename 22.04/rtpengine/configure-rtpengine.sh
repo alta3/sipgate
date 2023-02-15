fa30d6f10402bbdb14679f36e0fad210858ac11e
seaneon <35880398+seaneon@users.noreply.github.com>
2023-02-15 17:52:55 -0500

    Update configure-rtpengine.sh

--- 22.04/rtpengine/configure-rtpengine.sh
+++ 22.04/rtpengine/configure-rtpengine.sh
@@ -6,11 +6,11 @@ export RTPE_PORT_MIN=`cat ~/portmin`
 
 export RTPE_PORT_MAX=`cat ~/portmax`
 
-export TURN_CLOUD=$(nslookup 10.0.0.1 | grep -oP 'alpha|bravo')
+export MY_CLOUD=$(nslookup 10.0.0.1 | grep -oP 'alpha|bravo')
 
-export TURN_FQDN="turn.$TURN_CLOUD.alta3.com"
+export MY_FQDN="live.$MY_CLOUD.alta3.com"
 
-export MY_OUTSIDE_IP=$(dig +short $TURN_FQDN)
+export MY_OUTSIDE_IP=$(dig @8.8.8.8 $MY_FQDN +short )
 
 export MY_IP4_ADDR=$(ip a s ens3 | awk -F"[/ ]+" '/inet / {print $3}')
 
